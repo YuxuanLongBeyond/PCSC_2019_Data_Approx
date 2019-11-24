b2fd4efab07fb035e636a608ca91c321ae9a55b7
YuxuanLongBeyond <yuxuan.long@epfl.ch>
2019-11-24 18:15:10 +0100

    implement polynomial approx, piecewise approx

--- Approximator.cpp
+++ Approximator.cpp
@@ -7,30 +7,143 @@
 #include <cmath>
 #include "Approximator.h"
 
-Approximator::Approximator(std::vector<double>& data_x, std::vector<double>& data_y) {
-    int n1 = data_x.size();
-    int n2 = data_y.size();
+Approximator::Approximator(std::vector<double>& x, std::vector<double>& y) {
+    int n1 = x.size();
+    int n2 = y.size();
     if (n1 != n2) {
-        throw std::invalid_argument("Incompatible data size");
+        throw std::invalid_argument("Incompatible data size!");
     }
+
+
     N = n1;
     for (int i = 1; i < N; i ++) {
-        x[i] = data_x[i];
-        y[i] = data_y[i];
+        data_x[i] = x[i];
+        data_y[i] = y[i];
     }
 }
 
 std::vector<double> Approximator::polyfit(int degree) const {
-
+    if (degree < 0) {
+        throw std::invalid_argument("Degree should be positive!");
+    }
+    if (degree > N - 1) {
+        throw std::invalid_argument("Degree should be equal or smaller than N - 1!");
+    }
+    std::vector<double> w;
+    Matrix A(N, degree + 1);
+    for (int i = 0; i < N; i ++) {
+        for (int j = 0; j < degree + 1; j ++) {
+            A[i][j] = pow(data_x[i], j);
+        }
+    }
+    if (degree == N - 1) {
+        w = gauss_solve(A, data_y);
+    }
+    else {
+        w = least_squares(A, data_y, 0.0);
+    }
+    return w;
 }
 
 std::vector<double> Approximator::polyval(std::vector<double>& x_test, std::vector<double>& w) const {
+    int m = x_test.size();
+    int n = w.size();
+    Matrix A(m, n);
+    for (int i = 0; i < m; i ++) {
+        for (int j = 0; j < n; j ++) {
+            A[i][j] = pow(x_test[i], j);
+        }
+    }
+    std::vector<double> y_test;
+    y_test = A * w;
+    return y_test;
+}
 
+int Approximator::find_index(int start_index, double v) const {
+    if (start_index == -1) {
+        if (v < data_x[0]) {
+            // extrapolation
+            return start_index;
+        }
+        start_index += 1;
+    }
+    // v > data_x[start_index]
+    int index = start_index + 1;
+    while (index < N) {
+        if (v < data_x[index]) {
+            break;
+        }
+        index += 1;
+    }
+    return index - 1;
 }
 
 std::vector<double> Approximator::interp1(std::vector<double>& x_test) const{
+    std::vector<double> y_test;
+    std::vector<double> slope(N - 1);
+    for (int i = 0; i < N - 1; i ++) {
+        slope[i] = (data_y[i + 1] - data_y[i]) / (data_x[i + 1] - data_x[i]);
+    }
+    int index = -1; // start_index
+    double interp_out;
+    for (auto v: x_test) {
 
+        if (index < N - 1) {
+            index = find_index(index, v);
+            if (index == -1) {
+                // extrapolation
+                interp_out = data_y[0] + slope[0] * (v - data_x[0]);
+            }
+            else {
+                interp_out = data_y[index] + slope[index] * (v - data_x[index]);
+            }
+        }
+        else {
+            //extrapolation
+            interp_out = data_y[N - 1] + slope[N - 2] * (v - data_x[N - 1]);
+        }
+        y_test.push_back(interp_out);
+    }
+    return y_test;
 }
-std::vector<double> Approximator::spline(std::vector<double>& x_test) const {
 
+std::vector<double> Approximator::spline(std::vector<double>& x_test) const {
+    int m = 4 * (N - 1);
+    Matrix A(m, m);
+    std::vector<double> b(m);
+
+    // construct a linear system
+    b[0] = 0.0; b[1] = data_y[0];
+    A[0][0] = 1.0; A[0][4] = -1.0;
+    A[1][0] = pow(data_x[0], 3);
+    A[1][1] = pow(data_x[0], 2);
+    A[1][2] = data_x[0]; A[1][3] = 1.0;
+
+    int row = 2;
+    int col = 0;
+    double x, x_cube, x_sq;
+    for (int i = 1; i < N - 1; i ++) {
+        x = data_x[i];
+        x_cube = pow(x, 3);
+        x_sq = pow(x, 2);
+
+        A[row][col] = x_cube; A[row][col + 1] = x_sq; A[row][col + 2] = x; A[row][col + 3] = 1.0;
+        A[row + 1][col + 4] = x_cube; A[row + 1][col + 5] = x_sq; A[row + 1][col + 6] = x; A[row + 1][col + 7] = 1.0;
+
+        A[row + 2][col] = 3.0 * x_sq; A[row + 2][col + 1] = 2.0 * x; A[row + 2][col + 2] = 1.0;
+        A[row + 2][col + 4] = -3.0 * x_sq; A[row + 2][col + 5] = -2.0 * x; A[row + 2][col + 6] = -1.0;
+        A[row + 3][col] = 6.0 * x; A[row + 3][col + 1] = 2.0;
+        A[row + 3][col + 4] = -6.0 * x; A[row + 3][col + 5] = -2.0;
+
+        row += 4;
+        col += 4;
+    }
+
+    b[m - 2] = 0.0; b[m - 1] = data_y[N - 1];
+    A[m - 2][m - 8] = 1.0; A[m - 2][m - 4] = -1.0;
+    A[m - 1][m - 4] = pow(data_x[N - 1], 3);
+    A[m - 1][m - 3] = pow(data_x[N - 1], 2);
+    A[m - 1][m - 2] = data_x[N - 1]; A[m - 1][m - 1] = 1.0;
+
+    
 }
--- Approximator.h
+++ Approximator.h
@@ -12,13 +12,15 @@
 class Approximator {
 private:
 //    std::string method;
-    std::vector<double> x;
-    std::vector<double> y;
+    std::vector<double> data_x;
+    std::vector<double> data_y;
     int N;
 public:
-    Approximator(std::vector<double>& data_x, std::vector<double>& data_y);
+    Approximator(std::vector<double>& x, std::vector<double>& y);
     std::vector<double> polyfit(int degree) const;
     std::vector<double> polyval(std::vector<double>& x_test, std::vector<double>& w) const;
+
+    int find_index(int start_index, double v) const;
 
     std::vector<double> interp1(std::vector<double>& x_test) const;
     std::vector<double> spline(std::vector<double>& x_test) const;
